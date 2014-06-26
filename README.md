18cbf5326b92ec51c64adeb8568e650c4d898d7e
Charles Craig <ccraig13@gmail.com>
2014-06-26 14:36:55 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+tvguide2xml
+======================
+
+Converts the TV Guide listings page ( http://www.tvguide.com/Listings/ ) to XML. Exports to console.
---
+++ js/contentscript.js
@@ -0,0 +1,50 @@
+﻿var log = function(m) {
+  if ( false ) {
+    console.log(m);
+  }
+};
+
+$('.listings-pickers').append('<button style="background-color: limegreen;line-height:inherit;height:inherit;margin-top:0" id="convert-to-xml">Convert to XML</button>')
+
+var convertToXML = function() {
+
+  var rows = $('.listings-channel-row');
+  var xml = "<channels>\n";
+
+  $.each( rows, function(i,row) {
+
+    var channel = $(row).children('.listings-channel').text();
+    xml += "<channel>\n";
+    xml += "<name>"+ channel +"</name>\n";
+    log( channel );
+
+    var programs = $(row).children('.listings-program');
+
+    xml += "<programs>\n";
+
+    $.each( programs, function(i, program) {
+      var inner = $(program).children('.listings-program-inner');
+      var title = $(inner).children('.listings-program-title').text();
+      var time  = $(inner).children('.listings-program-time').text();
+
+      xml += "<program>\n<title>"+ title +"</title><time>"+time+"</time>\n</program>\n";
+
+      log( 'Title: ' + title + ' Time: ' + time );
+    });
+
+    xml += "</programs>\n";
+
+    xml += "</channel>\n";
+
+  });
+
+  xml += "</channels>\n";
+
+  return xml;
+
+};
+
+$('#convert-to-xml').click(function() {
+  var xml = convertToXML();
+  console.log( xml );
+});
